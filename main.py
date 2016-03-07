51fa53f4154c86e680314de494e4412e5985436f
Oleg Broslavsky <yalegko@gmail.com>
2016-03-07 12:16:02 +0600

    Add a bit more code to funcs that will work w/ database

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import json
+import os.path
+import sqlite3
+#import warcraftograph
 from flask import Flask, make_response, render_template
 app = Flask(__name__)
 
@@ -14,7 +17,22 @@ def get_secret():
 
 @app.route('/secret/<int:id>')
 def get_secret(id):
-    return render_template('secret.html')
+    fname = "static/cache/%d.jpg" % (id)
+    # If the secret hasn't been cached yet
+    if not os.path.isfile(fname):
+        c = db.cursor()
+        query = '''
+            SELECT name, secret FROM secrets
+            WHERE id = ?
+            '''
+        c.execute(query, (id))
+        secret_name, secret = c.fetchone()
+        if not secret:
+            id = "nosuchsecret"
+            secret_name = "No such secret!"
+        else:
+            #img = warcraftograph.encode(secret, fname)
+    return render_template('secret.html', name=secret_name, id=id)
 
 @app.route('/store')
 def store_secret():
@@ -26,12 +44,19 @@ def api_get_secret():
     c = db.cursor()
     query = '''
         SELECT count(*) FROM secrets
-        WHERE name = ? AND public = 0
+        WHERE name = ? AND public = 1
         '''
     c.execute(query.replace('?',secret_name))
-
-
-    secret_json = {}
+    if c.fetchone()[0] > 0:
+        c = db.cursor()
+        query = '''
+            SELECT secret FROM secrets
+            WHERE name = ? AND public = 1
+            '''
+        c.execute(query, (secret_name))
+        secret_json = {'secrets': [row[0] for row in c]}
+    else:
+        secret_json = {'secrets': []}
     return json.dumps(secret_json)
 
 @app.route('/api/store', methods = ['POST'])
